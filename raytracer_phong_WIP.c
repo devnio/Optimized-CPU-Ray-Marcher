24b3876fcb972e1380a3e65f0b8942e086254f1f
devnio <nihat_95@hotmail.it>
2020-03-18 01:19:34 +0100

    Reflection works more properly.. but still to test

--- raytracer_phong_WIP.c
+++ raytracer_phong_WIP.c
@@ -4,22 +4,24 @@
 #include <math.h>
 
 // MACROS
-#define MAX_RAY_DEPTH 1
+#define MAX_RAY_DEPTH 4
 #define OBJS_IN_SCENE 4
-
 
 /* ===============
  * Vector 3 - START
  * =============== */
 
-typedef struct {
+typedef struct
+{
     double x, y, z;
 } Vector3;
 
 Vector3 new_vector(double x, double y, double z)
 {
     Vector3 v;
-    v.x = x; v.y = y; v.z = z;
+    v.x = x;
+    v.y = y;
+    v.z = z;
     return v;
 }
 
@@ -56,7 +58,7 @@ double vec_norm(Vector3 v)
 Vector3 vec_normalized(Vector3 v)
 {
     double norm = vec_norm(v);
-    return vec_mult1(v, 1/norm);
+    return vec_mult1(v, 1 / norm);
 }
 
 double vec_dot(Vector3 v1, Vector3 v2)
@@ -64,7 +66,8 @@ double vec_dot(Vector3 v1, Vector3 v2)
     return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
-Vector3 vec_reflect(Vector3 v, Vector3 normal) {
+Vector3 vec_reflect(Vector3 v, Vector3 normal)
+{
     return vec_sub(v, vec_mult1(vec_mult1(normal, vec_dot(v, normal)), 2));
 }
 
@@ -75,7 +78,7 @@ Vector3 vec_reflect(Vector3 v, Vector3 normal) {
 /* ===============
  * Sphere - START
  * =============== */
-typedef struct 
+typedef struct
 {
     Vector3 c;
     double r;
@@ -90,17 +93,21 @@ double sphere_ray_intersection(Vector3 o, Vector3 dir, Sphere s)
 {
     Vector3 l = vec_sub(s.c, o);
     float tca = vec_dot(l, dir);
-    if (tca < 0) return -1;
+    if (tca < 0)
+        return -1;
 
     float d2 = vec_dot(l, l) - tca * tca;
-    if (d2 > s.r2) return -1;
+    if (d2 > s.r2)
+        return -1;
 
     float thc = sqrt(s.r2 - d2);
     float t0 = tca - thc;
     float t1 = tca + thc;
-    
-    if (t0 < 0) return t1;
-    else return t0;
+
+    if (t0 < 0)
+        return t1;
+    else
+        return t0;
     // check if t0 < 0
 }
 
@@ -118,17 +125,22 @@ double mix(double a, double b, double mix)
 
 double min(double a, double b)
 {
-    if (a < b) return a;
-    else return b;
+    if (a < b)
+        return a;
+    else
+        return b;
 }
 
 double max(double a, double b)
 {
-    if (a < b) return b;
-    else return a;
+    if (a < b)
+        return b;
+    else
+        return a;
 }
 
-Vector3 color_addWeighted(Vector3 colA, Vector3 colB, double weightA, double weightB) {
+Vector3 color_addWeighted(Vector3 colA, Vector3 colB, double weightA, double weightB)
+{
     double sum = weightA + weightB;
     Vector3 c;
     c.x = ((colA.x * weightA) + (colB.x * weightB)) / sum;
@@ -144,19 +156,20 @@ Vector3 color_blend(Vector3 colA, Vector3 colB, double weightA)
     Vector3 c;
     return color_addWeighted(colA, colB, wA, weightB);
 }
+
 /* ===============
  * HELPER - END
  * =============== */
 
-int bounces = 1;
 Vector3 trace(Vector3 o, Vector3 dir, Sphere sps[], int depth)
 {
 
     // SOME GLOBAL VARIABLES
-    float shininess = 80;
+
+    float shininess = 15;
     float specular = 0;
-    Vector3 ambientColor =  new_vector(0, 0, 0);//new_vector(0, 0, 0)
-    Vector3 finalColor = new_vector(0,0,0);
+    Vector3 ambientColor = new_vector(0, 0, 0); //new_vector(0, 0, 0)
+    Vector3 finalColor = new_vector(0, 0, 0);
 
     // CHECK INTERSECTION WITH SCENE
     float tnear = INFINITY;
@@ -173,9 +186,10 @@ Vector3 trace(Vector3 o, Vector3 dir, Sphere sps[], int depth)
             }
         }
     }
-    
+
     // No intersection case (return black)
-    if (!nearestSp) return new_vector(0, 0, 0);
+    if (!nearestSp)
+        return new_vector(0, 0, 0);
     float bias = 1e-4;
     Vector3 surfaceColor = nearestSp->surfCol;
 
@@ -185,31 +199,29 @@ Vector3 trace(Vector3 o, Vector3 dir, Sphere sps[], int depth)
     // Normal
     Vector3 N = vec_sub(phit, nearestSp->c);
     N = vec_normalized(N);
-    
-    // TODO: is this necessary?
-    // if (vec_dot(dir, N) > 0) 
-    // {
-    //     N = vec_mult1(N, -1);
-    // }
+
+    // TODO: in theory not necessary if normals are computed outwards
+    if (vec_dot(dir, N) > 0)
+    {
+        N = vec_mult1(N, -1);
+    }
 
     // Light dir
     Vector3 L = vec_sub(sps[OBJS_IN_SCENE].c, phit); // last element
     L = vec_normalized(L);
 
-
     // printf("Light dir: %f, %f, %f", L.x, L.y, L.z);
     // printf("Normal dir: %f, %f, %f", N.x, N.y, N.z);
 
-    if ((depth < MAX_RAY_DEPTH) && (nearestSp->refl > 0)) 
+    if ((depth < MAX_RAY_DEPTH) && (nearestSp->refl > 0))
     {
         // COMPUTE reflection value
-        Vector3 reflDir = vec_sub(dir, vec_mult1(vec_mult1(N, vec_dot(dir, N)), 2)); 
+        Vector3 reflDir = vec_sub(dir, vec_mult1(vec_mult1(N, vec_dot(dir, N)), 2));
         reflDir = vec_normalized(reflDir);
 
-        Vector3 reflectionCol = trace(vec_add(phit, vec_mult1(N, bias)), reflDir, sps, depth + 1);
+        Vector3 reflectedCol = trace(vec_add(phit, vec_mult1(N, bias)), reflDir, sps, depth + 1);
         //surfaceColor =  vec_add(reflectionCol, vec_mult1(surfaceColor, nearestSp->refl));  // color_blend(reflection, surfaceColor, nearestSp->refl);
-        finalColor = vec_mult1(reflectionCol, nearestSp->refl);
-        bounces++;
+        finalColor = vec_mult1(reflectedCol, nearestSp->refl);
     }
 
     // Before doing anything else check if shadow ray
@@ -218,36 +230,38 @@ Vector3 trace(Vector3 o, Vector3 dir, Sphere sps[], int depth)
         float t_shadow = sphere_ray_intersection(vec_add(phit, vec_mult1(N, bias)), L, sps[j]);
         if (t_shadow > 0)
         {
-            return ambientColor;
+            return vec_add(finalColor, ambientColor);
         }
     }
 
     // Lamber's cosine law
     float lambertian = max(vec_dot(N, L), 0.0);
-    if (lambertian > 0.0) {
+    if (lambertian > 0.0)
+    {
         // printf("Lambertian: %f", lambertian);
+        // Light reflected on normal
         Vector3 R = vec_reflect(vec_mult1(L, -1), N);
         Vector3 V = vec_normalized(vec_mult1(dir, -1));
-        
-        // specular term 
+
+        // specular term
         float specAngle = max(vec_dot(R, V), 0.0);
         specular = pow(specAngle, shininess);
     }
 
     Vector3 diffuseColor = vec_mult1(surfaceColor, lambertian);
     // if (nearestSp->refl > 0) diffuseColor = new_vector(0,0,0);
-    Vector3 specularColor = new_vector(255, 255, 255);
+    float specPow = 0.2;
+    Vector3 specularColor = new_vector(specPow, specPow, specPow);
     specularColor = vec_mult(sps[OBJS_IN_SCENE].emissionColor, vec_mult1(specularColor, specular));
     finalColor = vec_add(finalColor, vec_add(vec_add(ambientColor, diffuseColor), specularColor));
 
-
-    // float bias = 1e-4; // add some bias to the point from which we will be tracing 
-    // if (vec_dot(dir, nhit) > 0) 
+    // float bias = 1e-4; // add some bias to the point from which we will be tracing
+    // if (vec_dot(dir, nhit) > 0)
     // {
     //     nhit = vec_mult1(nhit, -1);
     // }
 
-    // if ((depth < MAX_RAY_DEPTH) && (nearestSp->refl > 0)) 
+    // if ((depth < MAX_RAY_DEPTH) && (nearestSp->refl > 0))
     // {
     //     // COMPUTE reflection value
     //     Vector3 reflDir = vec_sub(dir, vec_mult1(vec_mult1(nhit, vec_dot(dir, nhit)), 2));
@@ -261,7 +275,7 @@ Vector3 trace(Vector3 o, Vector3 dir, Sphere sps[], int depth)
     // float transmission = 1;
     // Vector3 lightDir = vec_sub(sps[OBJS_IN_SCENE].c, phit); // last element
     // lightDir = vec_normalized(lightDir);
-    
+
     // // CHECK FOR SHADOW RAY
     // for (int j = 0; j < OBJS_IN_SCENE; j++)
     // {
@@ -272,18 +286,17 @@ Vector3 trace(Vector3 o, Vector3 dir, Sphere sps[], int depth)
     //         break;
     //     }
     // }
-    
+
     // // IF NO SHADOW RAY - COMPUTE LIGHT CONTRIBUTION
     // if (transmission != 0)
     // {
     //     Vector3 lightColRatio = vec_mult1(nearestSp->surfCol, transmission);
     //     lightColRatio = vec_mult1(lightColRatio, max(0, vec_dot(nhit, lightDir)));
-    //     surfaceColor = vec_add(surfaceColor, vec_mult(lightColRatio, sps[OBJS_IN_SCENE].emissionColor)); 
+    //     surfaceColor = vec_add(surfaceColor, vec_mult(lightColRatio, sps[OBJS_IN_SCENE].emissionColor));
     // }
 
     return finalColor;
 }
-
 
 void render(Sphere sps[])
 {
@@ -304,19 +317,18 @@ void render(Sphere sps[])
     {
         for (unsigned x = 0; x < width; ++x)
         {
-            float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
-            float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
-            Vector3 dir = new_vector(xx, yy, -1);
+            float xx = 0 + (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+            float yy = 0 + (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+            Vector3 dir = new_vector(xx, yy, -0.7);
             dir = vec_normalized(dir);
 
             Vector3 px_col = trace(new_vector(0, 0, 0), dir, sps, 0);
-            px_col = vec_mult1(px_col, multiplier/bounces);  // TODO: should divide by a counter of successful bounces instead
-            bounces = 1;
+            px_col = vec_mult1(px_col, multiplier); // TODO: should divide by a counter of successful bounces instead
 
-            fprintf(f, "%d",     (int)(min(1, px_col.x) * 255));
-            fprintf(f, " %d",    (int)(min(1, px_col.y) * 255));
+            fprintf(f, "%d", (int)(min(1, px_col.x) * 255));
+            fprintf(f, " %d", (int)(min(1, px_col.y) * 255));
             fprintf(f, " %d \n", (int)(min(1, px_col.z) * 255));
-        }   
+        }
     }
 
     fclose(f);
@@ -324,16 +336,45 @@ void render(Sphere sps[])
 
 int main()
 {
-    Sphere sp0; sp0.c = new_vector(0, -10004, -20); sp0.r = 10000; sp0.r2 = 100000000; sp0.surfCol = new_vector(0.2, 0.3, 0.8); sp0.refl=0; sp0.emissionColor = new_vector(0,0,0);
-    Sphere sp1; sp1.c = new_vector(4, 0, -25); sp1.r = 3; sp1.r2 = 9; sp1.surfCol = new_vector(1, 0.32, 0.36); sp1.refl=0.5; sp1.emissionColor = new_vector(0,0,0);
-    Sphere sp2; sp2.c = new_vector(-4, 0, -15); sp2.r = 3; sp2.r2 = 9; sp2.surfCol = new_vector(0.3, 1, 0.36); sp2.refl=0; sp2.emissionColor = new_vector(0,0,0);
-    Sphere sp3; sp3.c = new_vector(0, 0, -40); sp3.r = 3; sp3.r2 = 9; sp3.surfCol = new_vector(0.65, 0.77, 0.97); sp3.refl=0; sp3.emissionColor = new_vector(0,0,0);
+    Sphere sp0;
+    sp0.c = new_vector(0, -10004, -20);
+    sp0.r = 10000;
+    sp0.r2 = 100000000;
+    sp0.surfCol = new_vector(0.2, 0.3, 0.8);
+    sp0.refl = 0;
+    sp0.emissionColor = new_vector(0, 0, 0);
+    Sphere sp1;
+    sp1.c = new_vector(4, 0, -25);
+    sp1.r = 3;
+    sp1.r2 = 9;
+    sp1.surfCol = new_vector(0.8, 0, 0);
+    sp1.refl = 0;
+    sp1.emissionColor = new_vector(0, 0, 0);
+    Sphere sp2;
+    sp2.c = new_vector(-4, 0, -15);
+    sp2.r = 3;
+    sp2.r2 = 9;
+    sp2.surfCol = new_vector(0.3, 1, 0.36);
+    sp2.refl = 0;
+    sp2.emissionColor = new_vector(0, 0, 0);
+    Sphere sp3;
+    sp3.c = new_vector(0, 0, -40);
+    sp3.r = 3;
+    sp3.r2 = 9;
+    sp3.surfCol = new_vector(0.2, 0.2, 0.97);
+    sp3.refl = 1;
+    sp3.emissionColor = new_vector(0, 0, 0);
     // Sphere sp4; sp4.c = new_vector(-5.5, 0, -15); sp4.r = 3; sp4.r2 = 9; sp4.surfCol = new_vector(0.9, 0.9, 0.9); sp4.emissionColor = new_vector(0,0,0);
 
     double em = 2;
-    Sphere sp5; sp5.c = new_vector(0, 20, -15); sp5.r = 3; sp5.r2 = 9; sp5.surfCol = new_vector(0, 0, 0); sp5.emissionColor = new_vector(em, em, em);
+    Sphere sp5;
+    sp5.c = new_vector(0, 20, -15);
+    sp5.r = 3;
+    sp5.r2 = 9;
+    sp5.surfCol = new_vector(0, 0, 0);
+    sp5.emissionColor = new_vector(em, em, em);
 
-    Sphere sps[OBJS_IN_SCENE+1];
+    Sphere sps[OBJS_IN_SCENE + 1];
     sps[0] = sp0;
     sps[1] = sp1;
     sps[2] = sp2;
