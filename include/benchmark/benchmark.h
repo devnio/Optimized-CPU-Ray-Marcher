e9819d4a3ecaafe02562523d909c1abf537a1e4c
Nihat Isik <nihat.isik@inf.ethz.ch>
2020-05-09 05:13:41 +0200

    Add support to call benchmark from terminal. Move create and save image out of render. Move macros to config.h

--- include/benchmark/benchmark.h
+++ include/benchmark/benchmark.h
@@ -13,14 +13,13 @@
 #endif
 
 // Function pointers to functions that we benchmark
-typedef void(*render_func_prot)(Scene, char*);
+typedef void(*render_func_prot)(Scene);
 typedef Vec3(*trace_func_prot)(Vec3, Vec3, Scene, int);
 
 // Benchmark functions
-double benchmark_render(render_func_prot f, SceneContainer sceneContainer);
-double benchmark_trace(trace_func_prot f, SceneContainer sceneContainer);
+void benchmark_render(render_func_prot f, SceneContainer sceneContainer);
+void benchmark_trace(trace_func_prot f, SceneContainer sceneContainer);
 
 void create_params_file(char* time, char* scneneName, char* dirName);
 char* create_dir_with_current_date(char *path, const char* prepend);
 char* create_dir(char* path, char* dirName);
-void write_measurm_to_file(FILE *f, char *s_n, unsigned int n, char *s_cycles, unsigned long long cycles);
