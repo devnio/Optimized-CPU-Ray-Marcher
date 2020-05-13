b94d0aba4407afbb27a298f846f15d5a11799da1
Nihat Isik <isikn@student.ethz.ch>
2020-05-14 01:24:25 +0200

    Small change for box

--- src/geometry/box.c
+++ src/geometry/box.c
@@ -12,14 +12,14 @@ Params are:
 double sdf_box(Vec3 p, double params[])
 {
   Vec3 q;
-  q.x = (p.x < 0) ? (p.x * -1 - params[0]) : (p.x - params[0]);
-  q.y = (p.y < 0) ? (p.y * -1 - params[1]) : (p.y - params[1]);
-  q.z = (p.z < 0) ? (p.z * -1 - params[2]) : (p.z - params[2]);
+  q.x = (p.x < 0) ? (-p.x - params[0]) : (p.x - params[0]);
+  q.y = (p.y < 0) ? (-p.y - params[1]) : (p.y - params[1]);
+  q.z = (p.z < 0) ? (-p.z - params[2]) : (p.z - params[2]);
 
-  // Vec3 zero = new_vector_one(0.0);
-  Vec3 zero;
-  set_vec_from_double(&zero, 0.0);
   Vec3 max_tmp; 
-  vec_max(&q, &zero, &max_tmp);
+  max_tmp.x = q.x < 0.0 ? 0.0 : q.x;
+  max_tmp.y = q.y < 0.0 ? 0.0 : q.y;
+  max_tmp.z = q.z < 0.0 ? 0.0 : q.z;
+
   return vec_norm(&max_tmp) + min(max(q.x, max(q.y, q.z)), 0.0);
 }
