69667727852665cbb479549e9f033e75c4948f3f
Alexandre Cavaleri <acavaler@student.ethz.ch>
2020-05-14 01:58:57 +0200

    fix torus

--- src/geometry/torus.c
+++ src/geometry/torus.c
@@ -12,5 +12,5 @@ double sdf_torus(Vec3 p, double params[])
 {
     double l = p.x * p.x + p.z * p.z;
     double sqrtL = sqrt(l);
-    return sqrt(l + p.y*p.y -(sqrtL << 1) + params[0]*params[0]) - params[1];
+    return sqrt(l + p.y*p.y -2*sqrtL + params[0]*params[0]) - params[1];
 }
