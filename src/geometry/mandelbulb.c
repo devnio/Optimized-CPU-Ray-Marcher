fe2fe52f2f251f6c260d72108b9d203126565bab
Qais El Okaili <qaise@student.ethz.ch>
2020-05-14 04:09:23 +0200

    improv by reducing another 3 flops

--- src/geometry/mandelbulb.c
+++ src/geometry/mandelbulb.c
@@ -14,7 +14,10 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
     double y = vec_p[1];
     double z = vec_p[2];
 
-    double m = x*x + y*y + z*z;
+    double x2 = x*x;
+    double y2 = y*y;
+    double z2 = z*z;
+    double m = x2 + y2 + z2;
 
 	double dz = 1.5;
     
@@ -25,9 +28,9 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
     double m4 = m2*m2;
     dz = 8.0*sqrt(m4*m2*m)*dz + 1.0;
 
-    double x2 = x*x; double x4 = x2*x2;
-    double y2 = y*y; double y4 = y2*y2;
-    double z2 = z*z; double z4 = z2*z2;
+    double x4 = x2*x2;
+    double y4 = y2*y2;
+    double z4 = z2*z2;
 
     double a = x2 + z2;
     double a2 = a*a;
@@ -45,7 +48,7 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
 
     x = vec_p[0] + 64.0*x*y*z*(x2 - z2)*d*(x4-6.0*x2*z2 + z4)*cb;
     // z = vec_p[2] +  -8.0*y*d*(x4*x4 - 28.0*x4*x2*z2 + 70.0*x4*z4 - 28.0*x2*z2*z4 + z4*z4)*c*b; // old
-    z = vec_p[2] + 8.0*y*d*( 28.0*x2*z2*(x4 + z4)  - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
+    z = vec_p[2] + 8.0*y*d*(28.0*x2*z2*(x4 + z4) - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
     y = vec_p[1] - 16.0*y2*a*d*d + c*c;
 
     x2 = x*x;
@@ -76,7 +79,7 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
     cb = c*b;
 
     x = vec_p[0] + 64.0*x*y*z*(x2 - z2)*d*(x4-6.0*x2*z2 + z4)*cb;
-    z = vec_p[2] + 8.0*y*d*( 28.0*x2*z2*(x4 + z4)  - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
+    z = vec_p[2] + 8.0*y*d*(28.0*x2*z2*(x4 + z4) - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
     y = vec_p[1] - 16.0*y2*a*d*d + c*c;
 
     x2 = x*x;
@@ -106,7 +109,7 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
     cb = c*b;
 
     x = vec_p[0] + 64.0*x*y*z*(x2 - z2)*d*(x4-6.0*x2*z2 + z4)*cb;
-    z = vec_p[2] + 8.0*y*d*( 28.0*x2*z2*(x4 + z4)  - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
+    z = vec_p[2] + 8.0*y*d*(28.0*x2*z2*(x4 + z4) - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
     y = vec_p[1] - 16.0*y2*a*d*d + c*c;
 
     x2 = x*x;
@@ -136,64 +139,10 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
     cb = c*b;
 
     x = vec_p[0] + 64.0*x*y*z*(x2 - z2)*d*(x4-6.0*x2*z2 + z4)*cb;
-    z = vec_p[2] + 8.0*y*d*( 28.0*x2*z2*(x4 + z4)  - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
+    z = vec_p[2] + 8.0*y*d*(28.0*x2*z2*(x4 + z4) - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
     y = vec_p[1] - 16.0*y2*a*d*d + c*c;
 
     m = x*x + y*y + z*z;
-
     
-
     return 0.25*log(m)*sqrt(m)/dz;
 }
-
-
-// double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
-// {
-//     double x = vec_p[0];
-//     double y = vec_p[1];
-//     double z = vec_p[2];
-
-//     double m = x*x + y*y + z*z;
-
-// 	double dz = 1.5;
-    
-// 	for(int i = 0; i < 4; i++)
-//     {
-//         double m2 = m*m;
-//         double m4 = m2*m2;
-// 		dz = 8.0*sqrt(m4*m2*m)*dz + 1.0;
-
-//         double x2 = x*x; double x4 = x2*x2;
-//         double y2 = y*y; double y4 = y2*y2;
-//         double z2 = z*z; double z4 = z2*z2;
-
-//         double h = x2 + z2;
-//         double h2 = h*h;
-//         double h4 = h2*h2;
-
-//         double j = (h == 0) ? 0.0 : 1.0/sqrt( h4*h2*h );
-//         // double j = 1.0/sqrt( h4*h2*h );
-//         double k = x4 + y4 + z4 - 6.0*y2*z2 - 6.0*x2*y2 + 2.0*z2*x2;
-//         double g = x2 - y2 + z2;
-
-//         x = vec_p[0] +  64.0*x*y*z*(x2 - z2) * g * (x4 - 6.0*x2*z2 + z4)*k*j;
-
-//         // z = vec_p[2] +  -8.0*y*g*(x4*x4 - 28.0*x4*x2*z2 + 70.0*x4*z4 - 28.0*x2*z2*z4 + z4*z4)*k*j;
-
-//         z = vec_p[2] +  -8.0*y*g*(x4*x4 - 28.0*x4*x2*z2 + 70.0*x4*z4 - 28.0*x2*z2*z4 + z4*z4)*k*j;
-
-//         // z = vec_p[2] +  8.0*y*g*(28.0*x2*z2*(x4 + z4) - 70.0*x4*z4 - z4*z4 - x4*x4)*k*j;
-
-//         // z = vec_p[2] +  8.0*y*g*(28.0*x2*z2*(x4 + z4) - (70.0*x4 + z4)*z4 - x4*x4)*k*j;
-
-//         // z = vec_p[2] +  -8.0*y*g*(x4*x4 + (70.0*z4- 28.0*x2*z2)*(x4 + z4))*k*j;
-
-//         y = vec_p[1] + -16.0*y2*h*g*g + k*k;
-
-//         m = x*x + y*y + z*z;
-// 		if( m > 256.0 )
-//             break;
-//     }
-
-//     return 0.25*log(m)*sqrt(m)/dz;
-// }
