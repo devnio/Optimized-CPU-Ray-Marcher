4d38ff48dd76e175713abef4b29d66ecadddd7d2
Qais El Okaili <qaise@student.ethz.ch>
2020-05-14 03:43:14 +0200

    analytic improvement, reduced flops

--- src/geometry/mandelbulb.c
+++ src/geometry/mandelbulb.c
@@ -32,18 +32,18 @@ double sdf_mandelbulb(const double vec_p[NR_VEC_ELEMENTS], double params[])
         double a2 = a*a;
         double a4 = a2*a2;
         
-        double b = (a == 0) ? 0.0 : 1.0/sqrt(a*a2*a4);
-        double c = x4 + y4 + z4 - 6.0*y2*z2 - 6.0*x2*y2 + 2.0*z2*x2;
+        double b = 1.0/sqrt(a*a2*a4); // double b = (a == 0) ? 0.0 : 1.0/sqrt(a*a2*a4);
+        double c = x4 + y4 + z4 - 6.0*y2*(z2 + x2) + 2.0*z2*x2;
+        // double c = x4 + y4 + z4 - 6.0*y2*z2 - 6.0*x2*y2 + 2.0*z2*x2;
         double d = x2 - y2 + z2;
-
-        x = vec_p[0] +  64.0*x*y*z*(x2-z2)*d*(x4-6.0*x2*z2+z4)*c*b;
-
-        // z = vec_p[2] +  -8.0*y*d*(x4*x4 - 28.0*x4*x2*z2 + 70.0*x4*z4 - 28.0*x2*z2*z4 + z4*z4)*c*b;
-        z = vec_p[2] +  8.0*y*d*( 28.0*x2*z2*(x4 + z4)  - 70.0*x4*z4  - z4*z4 - x4*x4 )*c*b;
         
-        y = vec_p[1] + -16.0*y2*a*d*d + c*c;
-        
+        double cb = c*b;
 
+        x = vec_p[0] + 64.0*x*y*z*(x2 - z2)*d*(x4-6.0*x2*z2 + z4)*cb;
+        // z = vec_p[2] +  -8.0*y*d*(x4*x4 - 28.0*x4*x2*z2 + 70.0*x4*z4 - 28.0*x2*z2*z4 + z4*z4)*c*b; // old
+        z = vec_p[2] + 8.0*y*d*( 28.0*x2*z2*(x4 + z4)  - (70.0*x4 + z4)*z4 - x4*x4 )*cb;
+        
+        y = vec_p[1] - 16.0*y2*a*d*d + c*c;
         m = x*x + y*y + z*z;
 		if( m > 256.0 )
             break;
