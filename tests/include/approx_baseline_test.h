ca1fb16f1dba51f9dc86d39ddccbb64aab1d7212
Nihat Isik <isikn@student.ethz.ch>
2020-05-15 04:10:55 +0200

    Add approximate test for images

---
+++ tests/include/approx_baseline_test.h
@@ -0,0 +1,17 @@
+#ifndef APPROX_BASELINE_H_
+#define APPROX_BASELINE_H_
+
+#include "greatest.h"
+
+SUITE(approx_baseline_test);
+TEST approx_compare_scene0();
+TEST approx_compare_mandelbulb();
+TEST approx_compare_shape0();
+TEST approx_compare_shape1();
+TEST approx_compare_shape2();
+TEST approx_compare_shape3();
+TEST approx_compare_shape4();
+
+TEST approx_compare_2_images();
+
+#endif //BASELINE_H_
